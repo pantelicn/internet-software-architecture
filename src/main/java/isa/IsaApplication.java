ffadeb8deeea870769b6a9b2ac920bfda69ed442
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2020-11-15 18:56:12 +0100

    Initialized Spring Boot project

---
+++ src/main/java/isa/IsaApplication.java
@@ -0,0 +1,13 @@
+package isa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isa/IsaApplicationTests.java
@@ -0,0 +1,13 @@
+package isa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
