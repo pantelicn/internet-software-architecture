95d74e74b47d149f54b88ab1dafb5cdfac051cf3
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2021-01-25 23:46:54 +0100

    Added vue-router and bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { router } from './router.js'
+import VueRouter from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+// https://icons.getbootstrap.com/ -> available icons
+Vue.use(IconsPlugin)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
