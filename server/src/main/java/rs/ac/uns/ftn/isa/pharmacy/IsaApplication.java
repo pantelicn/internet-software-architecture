2960256ca115b49a73feffda7346e54756c57618
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2021-01-27 14:34:56 +0100

    Added CORS support

--- server/src/main/java/rs/ac/uns/ftn/isa/pharmacy/IsaApplication.java
+++ server/src/main/java/rs/ac/uns/ftn/isa/pharmacy/IsaApplication.java
@@ -2,13 +2,27 @@ package rs.ac.uns.ftn.isa.pharmacy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class IsaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IsaApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:8081");
+			}
+		};
 	}
 
 }
