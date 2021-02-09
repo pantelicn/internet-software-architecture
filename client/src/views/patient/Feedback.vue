58f01daafd778ee0555a4d4ecf9676f4e19e74c2
Nikola Pantelic <nikola.pantelic@uns.ac.rs>
2021-02-09 13:46:00 +0100

    Rating frontend

--- client/src/views/patient/Feedback.vue
+++ client/src/views/patient/Feedback.vue
@@ -2,16 +2,67 @@
     <div class="container">
         <div class="row">
             <div class=col>
-                Rate dermatologists
+                <h5>Rate dermatologists</h5>
+                <table class="table table-striped table-dark">
+                    <tbody>
+                        <tr v-for="dermatologist in dermatologists" v-bind:key="dermatologist.id">
+                            <td>
+                                {{dermatologist.name + ' ' + dermatologist.lastName}}
+                            </td>
+                            <td>
+                                <StarRating v-model="dermatologist.rating" :star-size="20"/>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div class=col>
-                Rate pharmacists
+                <h5>Rate pharmacists</h5>
+                <table class="table table-striped table-dark">
+                    <tbody>
+                        <tr v-for="pharmacist in pharmacists" v-bind:key="pharmacist.id">
+                            <td>
+                                {{pharmacist.name + ' ' + pharmacist.lastName}}
+                            </td>
+                            <td>
+                                <StarRating v-model="pharmacist.rating" :star-size="20"/>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div class=col>
-                Rate drugs
+                <h5>Rate drugs</h5>
+                <table class="table table-striped table-dark">
+                    <tbody>
+                        <tr v-for="drug in drugs" v-bind:key="drug.id">
+                            <td>
+                                {{drug.name}}
+                            </td>
+                            <td>
+                                {{drug.manufacturer}}
+                            </td>
+                            <td>
+                                <StarRating v-model="drug.rating" :star-size="20"/>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div class=col>
-                Rate pharmacies
+                <h5>Rate pharmacies</h5>
+                <table class="table table-striped table-dark">
+                    <tbody>
+                        <tr v-for="pharmacy in pharmacies" v-bind:key="pharmacy.id">
+                            <td>
+                                {{pharmacy.name}}
+                            </td>
+                            <td>
+                                <StarRating v-model="pharmacy.rating" :star-size="20"/>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
@@ -20,6 +71,7 @@
 <script>
 import axios from 'axios'
 import { api } from '../../api.js'
+import StarRating from 'vue-star-rating'
 
 export default {
     data: function () {
@@ -29,6 +81,9 @@ export default {
             drugs: [],
             pharmacies: []
         }
+    },
+    components: {
+        StarRating
     },
     methods: {
         fetchRateable: function () {
